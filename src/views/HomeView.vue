6751954663f95b8c29bb45995c6d530987bbb315
Justine-Frigo <justine0912@gmail.com>
2024-09-04 15:43:54 +0200

    start quiz in vue js

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    </div>
+  
+  </template>
+  <script>
+  
+  export default {
+    name: 'HomeView.vue',
+    components: {
+    },
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() { },
+    methods: {}
+  }
+  </script>
+  
+  <style scoped>
+
+  
+  </style>
